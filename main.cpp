e6f3d62a1d0ff333da85e3bb1efd7ade4a77a3a4
amax3se <105502045+amax3se@users.noreply.github.com>
2026-06-16 20:51:05 +0300

    add adding words function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello!";
+    map<string, string> words;
+    string word, translation;
+    int wordsAm;
+
+    cout << "How much words do you want to add? ";
+    cin >> wordsAm;
+    cout << endl;
+    cout << "Please, enter the words" << endl;
+    
+    for (int i=0; i<wordsAm; i++) {
+        cout << "Enter the word: ";
+        cin >> word;
+        cout << "Enter the translation: ";
+        cin >> translation;
+
+        words[word] = translation;
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "Great! There are your words: " << endl;
+    for (const auto& pair : words) {
+        cout << pair.first << " - " << pair.second << endl;
+    }
 
     return 0;
 }
