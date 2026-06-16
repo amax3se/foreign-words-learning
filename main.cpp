5b791303dba890ff2afcda71caaa5aaa05403d82
amax3se <105502045+amax3se@users.noreply.github.com>
2026-06-16 21:23:23 +0300

    add commands

--- main.cpp
+++ main.cpp
@@ -3,37 +3,58 @@
 #include <string>
 using namespace std;
 
-int main() {
-    map<string, string> words;
-    string word, translation;
-    int wordsAm;
-
-    // Greatings
-    cout << "Hello! I will help you learn some foriegn words. My commands: " << endl;
+void commandShowing() { // showing commands
+    cout << "My commands are: " << endl;
     cout << "/newWords - add new words" << endl;
     cout << "/show - show your words" << endl;
     cout << "/cards - begin learning" << endl;
+}
 
-    
-    cout << "How much words do you want to add? ";
-    cin >> wordsAm;
-    cout << endl;
-    cout << "Please, enter the words" << endl;
-    
-    for (int i=0; i<wordsAm; i++) {
-        cout << "Enter the word: ";
-        cin >> word;
-        cout << "Enter the translation: ";
-        cin >> translation;
+int main() {
+    map<string, string> words;
+    string command;
 
-        words[word] = translation;
-        cout << endl;
-    }
+    // greetings
+    cout << "Hello! I will help you learn some foriegn words. ";
+    commandShowing();
 
-    cout << endl;
-    cout << "Great! There are your words: " << endl;
-    for (const auto& pair : words) {
-        cout << pair.first << " - " << pair.second << endl;
+    // commands 
+    while (true) {
+        cout << "Enter command: ";
+        cin >> command;
+        if (command == "/newWords") {  // add new words
+            int wordsAm;
+            string word, translation;
+
+            cout << "How much words do you want to add? ";
+            cin >> wordsAm;
+            cout << endl;
+            cout << "Please, enter the words" << endl;
+            
+            for (int i=0; i<wordsAm; i++) {
+                cout << "Enter the word: ";
+                cin >> word;
+                cout << "Enter the translation: ";
+                cin >> translation;
+
+                words[word] = translation;
+                cout << endl;
+            }
+            continue;
+        } else if (command == "/show") {  // show your words
+            cout << "Great! There are your words: " << endl;
+            for (const auto& pair : words) {
+                cout << pair.first << " - " << pair.second << endl;
+            }
+            continue;
+        } else if (command == "/cards") { // learning neew words with cards
+            /* There will be cards func */ 
+            continue;
+        } else { 
+            cout << "I havn't got that command yet. " << endl; 
+            commandShowing();
+            continue;
+        }
     }
 
     return 0;
